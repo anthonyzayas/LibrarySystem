32b20ccb656549258005593f40e19199c6f000bd
zayas <anthonyczayas@gmail.com>
2017-03-02 11:08:16 -0500

    Updated Gui form

--- src/io/zayas/View/Form.java
+++ src/io/zayas/View/Form.java
@@ -1,24 +1,76 @@
 package io.zayas.View;
 
+import javax.xml.soap.Text;
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 
 /**
  * Created by zayas on 3/2/17.
  */
-public class Form extends Frame{
+public class Form extends Frame implements WindowListener, ActionListener{
+
+    protected TextField title = new TextField(20);
+    protected TextField author = new TextField(20);
+    protected TextField genre = new TextField(20);
+    protected TextField date = new TextField(20);
+
+    Button submit;
 
     public Form(){
+        setLayout(new FlowLayout());
         setTitle("Library System");
         setSize(700, 700);
+        isVisible();
 
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                super.windowClosing(e);
-            }
-        });
+        addWindowListener(this);
+        add(title);
+        add(author);
+        add(genre);
+        add(date);
+
+        submit = new Button("Submit");
+        add(submit);
+        submit.addActionListener(this);
+
     }
 
+    @Override
+    public void windowOpened(WindowEvent e) {
+
+    }
+
+    public void windowClosing(WindowEvent e){
+        dispose();
+        System.exit(0);
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
